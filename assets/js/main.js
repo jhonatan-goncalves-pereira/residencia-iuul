96ed961f6b9aecd91b717ccce422c68bfb3af14e
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-03-22 09:14:48 -0300

    :refactor: Identação do código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var links = document.querySelectorAll('.nav-list a');
 
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            links.forEach(function(link) {
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            links.forEach(function (link) {
                 link.parentElement.classList.remove('activeBt');
             });
 
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // No clique do menu móvel
-document.querySelector('.mobile-menu').addEventListener('click', function() {
+document.querySelector('.mobile-menu').addEventListener('click', function () {
     // Adiciona ou remove a classe .active do elemento .nav-list
     document.querySelector('.nav-list').classList.toggle('active');
 });
